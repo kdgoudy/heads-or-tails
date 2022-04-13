a7ad5a5ad5b105c9e2f78e291d24637c7648fafe
Koki <kgoudy64@gmail.com>
2022-04-13 15:32:15 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#import random module
+import random
+
+#create variable 
+random_side = random.randint(0,1)
+
+#create if statement to determine if heads or tails
+if random_side == 1:
+    print("Heads")
+else:
+    print("Tails")
